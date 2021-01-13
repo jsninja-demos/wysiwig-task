54e46d6bd018e52542a3e4074d6ea19267babe82
Alexei <alexeiOrlov.dev@gmail.com>
2021-01-13 17:52:34 +0300

    added unwrapDecorator

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,33 +1,50 @@
-import {createTemplate, isDecorator} from "./template";
+import { createTemplate, isDecorator } from "./template";
 
-export function makeDecorator( editor:HTMLDivElement, decoratorName: string, style: string, tagName:string= "span") {
-    const highlight = window.getSelection()!;
+export function makeDecorator(
+  editor: HTMLDivElement,
+  decoratorName: string,
+  style: string,
+  tagName: string = "span"
+) {
+  const highlight = window.getSelection()!;
 
-    if(highlight.isCollapsed){
+  if (highlight.isCollapsed) {
+    return;
+  }
+
+  const { anchorNode, anchorOffset, focusNode, focusOffset } = highlight;
+
+  const range = highlight.getRangeAt(0);
+
+  // если нода одинаковая
+  if (anchorNode === focusNode) {
+    // если выделена вся нода
+    if (anchorOffset === 0 && focusOffset === 0) {
+      const parent = anchorNode!.parentNode as Element;
+
+      const decorator = isDecorator(anchorNode!.parentNode);
+      console.log(decorator);
+
+      if (decorator) {
+        range.insertNode(unwrapDecorator(parent));
         return;
+      }
     }
+  }
 
-    const {anchorNode,focusNode} = highlight;
+  const template = createTemplate(
+    tagName,
+    decoratorName,
+    style,
+    highlight.toString()
+  );
 
-    const range = highlight.getRangeAt(0);
+  range.deleteContents();
+  range.insertNode(template);
+}
 
-
-
-    // если нода одинаковая
-    if(anchorNode===focusNode){
-        const parent =anchorNode!.parentNode as Element;
-
-        if(isDecorator(anchorNode!.parentNode)){
-            const inner = parent.innerHTML;
-            parent.remove()
-            console.log(parent)
-            range.insertNode(document.createTextNode(inner));
-            return;
-        }
-
-    }
-    const template = createTemplate(tagName,decoratorName,style,highlight.toString());
-    range.deleteContents()
-    range.insertNode(template);
-
+function unwrapDecorator(el: Element): Text {
+  const inner = el.innerHTML;
+  el.remove();
+  return document.createTextNode(inner);
 }
