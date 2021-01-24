59ae1dd095ea405f4e3e3aafe18b2cb27865b07d
Alexei <alexeiOrlov.dev@gmail.com>
2021-01-24 22:00:07 +0300

    fix(decorators): added tagName for hed1Decorator and hed2Decorator

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,14 @@ const [hed1, hed2, bold, italic] = controls;
 
 const italicDecorator = createDecorator("italic", { className: "italic-text" });
 const boldDecorator = createDecorator("bold", { className: "bold-text" });
-const hed1Decorator = createDecorator("header1", { className: "header1-text" });
-const hed2Decorator = createDecorator("header2", { className: "header2-text" });
+const hed1Decorator = createDecorator("header1", {
+  className: "header1-text",
+  tagName: "h1",
+});
+const hed2Decorator = createDecorator("header2", {
+  className: "header2-text",
+  tagName: "h2",
+});
 
 new Editor(editorRef)
   .addDecorator(hed1, hed1Decorator)
